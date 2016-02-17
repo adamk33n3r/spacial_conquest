a3bd6bc6353c1f041955c657ac65dbb206880e1a
Adam Keenan <adam.g.keenan@gmail.com>
2016-02-17 10:22:17 -0600

    Remove typings

--- client/typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser/ambient/angular/angular.d.ts" />
-/// <reference path="browser/ambient/jquery/jquery.d.ts" />
-/// <reference path="browser/ambient/socket.io-client/socket.io-client.d.ts" />
--- client/typings/main.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="main/ambient/angular/angular.d.ts" />
-/// <reference path="main/ambient/jquery/jquery.d.ts" />
-/// <reference path="main/ambient/socket.io-client/socket.io-client.d.ts" />
