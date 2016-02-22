d794561110a3d387ea9c5a53b28aa0b820437558
Shane DeSeranno <tyren@syntheticstream.com>
2016-02-22 05:18:19 +0000

    Try wiring up Phaser

--- client/app/routes/home/home.controller.ts
+++ client/app/routes/home/home.controller.ts
@@ -4,15 +4,20 @@
 import * as Messages from '../../../../shared/Messages';
 
 import angular = require('angular');
+
 angular.module('spacial_conquest')
 .controller('HomeController', class {
     test: string;
     username: string;
     socket: SocketIOClient.Socket;
+    game: Phaser.Game;
+
     constructor ($http: ng.IHttpService) {
         this.test = 'This is a string defined in the controller!';
 
         this.socket = io();
+
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameCanvas', { preload: this.preload, create: this.create });
 
         this.socket.on(Messages.NewUserMessage.type, function (data: Messages.NewUserMessage) {
             console.log('New User Message: ' + data.username);
@@ -28,6 +33,15 @@ angular.module('spacial_conquest')
     sendMessage (message: Messages.Message) {
         console.log('Sending:', message);
         this.socket.emit(message.type, message);
+    }
+
+    preload() {
+        this.game.load.image('top-secret', 'images/top-secret.jpg');
+    }
+
+    create() {
+        let topSecret: Phaser.Sprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'top-secret');
+        topSecret.anchor.setTo(0.5, 0.5);
     }
 });
 
