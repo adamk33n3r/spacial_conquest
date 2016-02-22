9099de6581ef1e215e692a81433143c0ca701a58
Shane DeSeranno <tyren@syntheticstream.com>
2016-02-22 06:13:32 +0000

    Revert "Try wiring up Phaser"
    This reverts commit d794561110a3d387ea9c5a53b28aa0b820437558.

--- client/typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser/ambient/angular-ui-router/angular-ui-router.d.ts" />
-/// <reference path="browser/ambient/angular/angular.d.ts" />
-/// <reference path="browser/ambient/jquery/jquery.d.ts" />
-/// <reference path="browser/ambient/phaser/phaser.d.ts" />
-/// <reference path="browser/ambient/socket.io-client/socket.io-client.d.ts" />
